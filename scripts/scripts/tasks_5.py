9bd8c41eb4c835e9fa1b3a9ee1bd982b87fa0099
Vyacheslav Krachkov <vskrachkov@gmail.com>
2017-01-14 22:53:12 +0200

    add tasks_5.py

---
+++ scripts/scripts/tasks_5.py
@@ -0,0 +1,16 @@
+"""
+tasks_5.py
+==========
+
+The following exercises are designed to consolidate the understanding
+of the inner joins between tables.
+
+"""
+
+from core.common import BaseExercises
+
+
+class Exercises(BaseExercises):
+    def task51(self):
+        """"""
+        self.ex("")
