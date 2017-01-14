c373c860d5aa6bc015f8c79e8f8c555c0cec5fd2
Vyacheslav Krachkov <vskrachkov@gmail.com>
2017-01-14 22:39:19 +0200

    refactor project structure

--- scripts/interactive.py
+++ scripts/interactive.py
@@ -1,40 +1,43 @@
-from common import ex_
-
+from scripts.core.common import ex_
 
 db = input('Choose DB ("`test` or `bank`").'
-		   'By default uses `test` >')
+           'By default uses `test` >')
 
 if db == 'bank':
-	from common import con_bank as con
+    from scripts.core.common import con_bank as con
 else:
-	from common import con_test as con
+    from scripts.core.common import con_test as con
 
 
-def take_until(
-	previous_line=None, end=';', input_mark='--> ', exit=['q', 'exit']):
-	"""Takes lines from command line until one of them ends with 
-	value of :end: parameter.
-	Returns lines joined by empty spaces.
-	"""
-	def check_prev_line(prev, line):
-		if prev:
-			line = ' '.join((prev, line))
-		return line
+def take_until(previous_line=None,
+               end=';',
+               input_mark='--> ',
+               exit_cmd=('q', 'exit')):
+    """Takes lines from command line until one of them ends with
+    value of :end: parameter.
+    Returns lines joined by empty spaces.
+    """
 
-	line = input(input_mark)
-	if line in exit:
-		return 'E_X_I_T'
-	if line and (line[-1] == end):
-		return check_prev_line(previous_line, line)
-	else:
-		return take_until(previous_line=check_prev_line(previous_line, line))
+    def check_prev_line(prev, line):
+        if prev:
+            line = ' '.join((prev, line))
+        return line
+
+    line = input(input_mark)
+    if line in exit_cmd:
+        return 'E_X_I_T'
+    if line and (line[-1] == end):
+        return check_prev_line(previous_line, line)
+    else:
+        return take_until(previous_line=check_prev_line(previous_line, line))
+
 
 while 1:
-	sql = take_until()
-	if sql == 'E_X_I_T':
-		break
-	try:
-		for line in ex_(sql, con=con):
-			print(line)
-	except Exception as err:
-		print(err)
+    sql = take_until()
+    if sql == 'E_X_I_T':
+        break
+    try:
+        for line in ex_(sql, con=con):
+            print(line)
+    except Exception as err:
+        print(err)
